9ed54b687e2b7cdc254c98bf3f96031119c76a4f
Sagar Ghimire <gmrsagar@gmail.com>
2020-09-23 16:58:53 +1000

    Add mongo schema and pusher

--- server.js
+++ server.js
@@ -2,18 +2,76 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import Pusher from "pusher";
+import dbModel from "./dbModel.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 8080;
+const pusher = new Pusher({
+  appId: "1077922",
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: "ap1",
+  useTLS: true,
+});
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
 // db config
+const connection_url = `mongodb+srv://gmrsagar:${process.env.DB_PASS}@cluster0.kj7l8.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(connection_url, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log(`DB is connected`);
+
+  const changeStream = mongoose.connection.collection("posts").watch();
+  changeStream.on("change", (change) => {
+    console.log("Change Triggered");
+    console.log(change);
+
+    if (change.operationType === "insert") {
+      const postDetails = change.fullDocument;
+      pusher.trigger("posts", "inserted", {
+        user: postDetails.user,
+        caption: postDetails.caption,
+        image: postDetails.image,
+      });
+    } else {
+      console.log("Error triggering pusher");
+    }
+  });
+});
+
 // api routes
 app.get("/", (req, res) => res.status(200).send("live"));
+
+app.post("/upload", (req, res) => {
+  const body = req.body;
+
+  dbModel.create(body, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+app.get("/sync", (req, res) => {
+  dbModel.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 // listener
 app.listen(port, () => console.log(`listening on port: ${port}`));
