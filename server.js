f72aa0d401715acaf418abce4a6fdc828bd2ebaf
Sagar Ghimire <gmrsagar@gmail.com>
2020-09-23 15:07:43 +1000

    Initiate server

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import Pusher from "pusher";
+
+// app config
+const app = express();
+const port = process.env.PORT || 8080;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// db config
+// api routes
+app.get("/", (req, res) => res.status(200).send("live"));
+
+// listener
+app.listen(port, () => console.log(`listening on port: ${port}`));
